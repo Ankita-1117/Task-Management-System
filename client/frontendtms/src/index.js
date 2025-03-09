78fda10f5780f4e6f2e605a3bea319b1e04d2887
Ankita Bakshi <amiankita2019@gmail.com>
2025-03-09 17:34:58 +0530

    Initial Commit

---
+++ client/frontendtms/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+  <React.StrictMode>
+    
+    <BrowserRouter> {/* Wrap App in BrowserRouter */}
+      <App />
+    </BrowserRouter>
+ 
+  </React.StrictMode>
+  </Provider>
+);
+
+reportWebVitals();
